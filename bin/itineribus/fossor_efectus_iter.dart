6fcc180fe3575c2ae27fa250f2284f7b9a156b44
notyodeas <notyodeasnotpurples@gmail.com>
2024-05-23 23:55:13 +0200

    morezeross

--- bin/itineribus/fossor_efectus_iter.dart
+++ bin/itineribus/fossor_efectus_iter.dart
@@ -30,7 +30,7 @@ Future<Response> fossorEfectus(Request req) async {
   FossorPraecipuus fp = FossorPraecipuus();
   fp.accipere(    
     efectus: true, 
-    maxime: 100000, 
+    maxime: 10000000, 
     llt: par!.liberTransactions, 
     lft: par!.fixumTransactions, 
     let: par!.expressiTransactions, 
